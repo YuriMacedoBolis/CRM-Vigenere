792029a3d66be7100fb56bc0369769561dc7ca7a
Yuri Macedo Bolis <yuribolis2203@gmail.com>
2026-06-23 22:43:36 -0300

    Update CrmVigenereApplication.java

--- crm-vigenere/src/main/java/br/com/sgc/CrmVigenereApplication.java
+++ crm-vigenere/src/main/java/br/com/sgc/CrmVigenereApplication.java
@@ -1,13 +1,26 @@
 package br.com.sgc;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import br.com.sgc.view.TelaLogin;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class CrmVigenereApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrmVigenereApplication.class, args);
-	}
+    public static void main(String[] args) {
+        
+        // 1. Configura o Spring Boot para permitir a abertura de telas do Java Swing
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(CrmVigenereApplication.class);
+        builder.headless(false); 
+        
+        // 2. Liga o Servidor da API (O motor do backend)
+        builder.run(args);
+        
+        // 3. Assim que o servidor ligar com sucesso, ele abre a Tela de Login automaticamente!
+        java.awt.EventQueue.invokeLater(() -> {
+            TelaLogin tela = new TelaLogin();
+            tela.setVisible(true);
+        });
+    }
 
 }
